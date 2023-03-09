91e9e8fc5d741f441f3c62e5b88030a220574038
lathief <latifmukhtarul@ce.student.pens.ac.id>
2023-03-09 12:43:33 +0700

    second commit

--- src/main/java/com/XYZ/Karyawan/KaryawanApplication.java
+++ src/main/java/com/XYZ/Karyawan/KaryawanApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KaryawanApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KaryawanApplication.class, args);
 	}
-
 }
